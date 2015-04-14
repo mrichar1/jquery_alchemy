054fa2fb2dade9bcd900eaeed44ba929c84a6825
Matthew Richardson <M.Richardson@ed.ac.uk>
2015-04-14 12:27:32 +0100

    setup.py now python3 compatible

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_git_version():
                              stdout=subprocess.PIPE)
         return p.communicate()[0].split('\n')[0].strip()
     except Exception as e:
-        print e
+        print(e)
         return None
 
 
